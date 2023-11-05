3bf18ceef7c9189f0172968ab5f49d6e999572a7
Mekhrojbek <islomovmekhrojbek@gmail.com>
2023-11-05 18:08:09 +0500

    adding animation typing/erasing

--- src/Header.vue
+++ src/Header.vue
@@ -1,37 +1,134 @@
 <template>
-  <div class="container header">
+  <div class="header">
     <Particles />
-    <header>
-      <div class="header__content">
-        <div class="violet__img--wrapper">
-          <div class="violet-dot" />
-          <div class="violet-line" />
-        </div>
-
-        <div>
-          <div class="header__title">
-            Hello,I'm
-            <h1 class="header-name">Kamronbek</h1>
+    <div class="container">
+      <header>
+        <div class="header__content">
+          <div class="violet__img--wrapper">
+            <div class="violet-dot" />
+            <div class="violet-line" />
           </div>
 
-          <p class="header__description">Frontend developer</p>
+          <div>
+            <div class="header__title">
+              Hello, I'm
+              <h1 class="header-name">
+                {{ displayText }}
+                <span
+                  class="typed-cursor"
+                  :class="{ 'cursor-blink': cursorVisible }"
+                >
+                  |</span
+                >
+              </h1>
+            </div>
+
+            <p class="header__description">Frontend developer</p>
+          </div>
         </div>
+
+        <img
+          class="header__img"
+          src="./assets/main-img.png?format=webp"
+          alt="header image"
+        />
+
+        <img
+          class="header__img--min"
+          src="https://www.monitorteknologi.com/wp-content/uploads/2020/10/Apa-Itu-Programming-Pemrograman-1170x702.jpg?format=webp"
+          alt="header image for small screens"
+        />
+      </header>
+
+      <div class="social__link">
+        <a
+          class="social__btn"
+          href="/"
+          v-for="social in socials"
+          :key="social.name"
+          aria-label="to direct to my social account"
+        >
+          <span class="social__icon">
+            <i :class="social.icon"></i>
+          </span>
+
+          <span class="icon__name">{{ social.name }}</span>
+        </a>
       </div>
-
-      <img class="header__img" src="./assets/main-img.png" />
-
-      <img
-        class="header__img--min"
-        src="https://www.monitorteknologi.com/wp-content/uploads/2020/10/Apa-Itu-Programming-Pemrograman-1170x702.jpg"
-      />
-    </header>
+    </div>
   </div>
 </template>
+
 <script>
 import Particles from './Particles.vue';
+
 export default {
   components: {
     Particles,
+  },
+  data() {
+    return {
+      socials: [
+        {
+          name: 'Telegram',
+          icon: 'fa-brands fa-telegram',
+        },
+        {
+          name: 'LinkedIn',
+          icon: 'fa-brands fa-linkedin',
+        },
+        {
+          name: 'Twitter',
+          icon: 'fa-brands fa-twitter',
+        },
+        {
+          name: 'Github',
+          icon: 'fa-brands fa-github',
+        },
+      ],
+      text: 'Kamronbek',
+      displayText: '',
+      cursorVisible: true,
+      typingInterval: 200,
+      eraseInterval: 100,
+      cursorBlinkInterval: 600,
+    };
+  },
+  methods: {
+    typeAndErase() {
+      let isTyping = true;
+      let index = 0;
+
+      const typeEraseInterval = setInterval(
+        () => {
+          if (isTyping) {
+            this.displayText = this.text.slice(0, index);
+            index++;
+
+            if (index > this.text.length) {
+              isTyping = false;
+              this.toggleCursor();
+            }
+          } else {
+            this.displayText = this.text.slice(0, index);
+            index--;
+
+            if (index === 0) {
+              isTyping = true;
+              this.toggleCursor();
+            }
+          }
+        },
+        isTyping ? this.typingInterval : this.eraseInterval
+      );
+    },
+    toggleCursor() {
+      this.cursorVisible = !this.cursorVisible;
+    },
+  },
+  mounted() {
+    this.typeAndErase();
+    setInterval(this.toggleCursor, this.cursorBlinkInterval);
   },
 };
 </script>
@@ -40,6 +137,8 @@ header {
   display: flex;
   gap: 10px;
   flex-direction: column;
+  /* position: relative;
+  height: 100%; */
 }
 .header__content {
   display: flex;
@@ -57,7 +156,7 @@ header {
   font-size: 84px;
   font-weight: 700;
   margin: 0;
-  letter-spacing: 2px;
+  letter-spacing: 1.5px;
 }
 .header__description {
   font-size: 36px;
@@ -85,8 +184,69 @@ header {
     rgba(60, 51, 80, 0) 100%
   );
 }
+.typed-cursor {
+  color: var(--violet);
+  font-weight: normal;
+  position: relative;
+}
+.cursor-blink {
+  animation: cursor-blink 1s infinite;
+}
+
+@keyframes cursor-blink {
+  0%,
+  100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+}
 .header__img--min {
   display: none;
+}
+.social__link {
+  display: flex;
+  padding-top: 40px;
+  padding-bottom: 20px;
+  align-items: center;
+  justify-content: center;
+}
+.social__btn {
+  border-radius: 50px;
+  background-color: var(--main-bg);
+  border: 0.5px solid var(--default-color);
+  transition: all 0.4s ease;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+}
+.social__btn:not(:last-child) {
+  margin-right: 20px;
+}
+.social__icon {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  transition: all 0.4s ease;
+}
+.icon__name {
+  font-weight: 600;
+  color: #fdfeff;
+  overflow: hidden;
+  max-width: 0;
+  transition: all 0.5s ease;
+}
+.social__btn:hover .icon__name {
+  max-width: 140px;
+}
+.social__link .social__btn:hover {
+  padding-right: 15px;
 }
 @media (max-width: 1100px) {
   .header__title {
@@ -115,22 +275,37 @@ header {
   }
 }
 @media (max-width: 500px) {
-  header {
+  /* header {
     align-items: center;
-  }
+  } */
 }
 @media (max-width: 450px) {
   .header__content {
     gap: 10px;
   }
   .header-name {
-    font-size: 40px;
+    font-size: 36px;
   }
   .header__description {
     font-size: 24px;
   }
   .header__title {
     font-size: 20px;
+  }
+  .social__btn:not(:last-child) {
+    margin-right: 16px;
+  }
+  .social__icon {
+    width: 40px;
+    height: 40px;
+    border-radius: 40px;
+  }
+  .social__btn:hover .icon__name {
+    font-size: 14px;
+    max-width: 120px;
+  }
+  .social__link .social__btn:hover {
+    padding-right: 10px;
   }
 }
 </style>
